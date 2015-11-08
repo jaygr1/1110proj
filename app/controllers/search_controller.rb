b4d9b397b2cadcfbce635a6b30ce4f912cdcecd6
eenymini <hhl262@nyu.edu>
2015-11-08 00:09:15 -0500

    Did a little temporary formatting and also made it so two events are selected randomly. Formatting still needs work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,14 +6,22 @@ class SearchController < ApplicationController
   def index
     user_search = ["keywords", "zip_code"]
     new_object = []
+    @resultarray = []
     user_search.map do |search|
       if params[search] && params[search].length > 1
         new_object << "&#{search}=#{params[search]}"
       end
-      @results = Search.events(new_object)
-     
+      @results = Search.events(new_object)    
     end 
   end
+
+  # def randomevent
+  #   @results["events"]["event"].collect do |result|
+  #     @pulledevents = []
+  #     @pulledevents << result 
+  #     binding.pry
+  #   end
+  # end
 
 end
 
