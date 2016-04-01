a1a1b3724040aa896367bf6bc254b8d395fe77be
github username <hhl262@nyu.edu>
2016-04-01 14:26:34 -0400

    access api key via env variables, not loading yml file

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,10 +5,10 @@ require 'pry'
 
 class Search
   def self.events(details)
-  keys = YAML.load_file('application.yml')
-  key = keys['eventful_api_key']
+  # keys = ENV[""]
+  # key = keys['eventful_api_key']
 
-  response = open("http://api.eventful.com/json/events/search?#{details.join}&t=This+Weekend&app_key=#{key}")
+  response = open("http://api.eventful.com/json/events/search?#{details.join}&t=This+Weekend&app_key=#{ENV['eventful_api_key']}")
   hash = JSON.load(response)
   end
 end
